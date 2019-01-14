0bde79a6eaf4b8e9c97c84885d20f282a5ecd61a
nebmesemaou <cagricinar@gmail.com>
2019-01-14 21:17:55 +0000

    Update Config.php

--- classes/Config.php
+++ classes/Config.php
@@ -39,7 +39,7 @@ class Config
      *
      * @var array
      */
-    public $params = ['--simulate', '--no-check-certificate', '--no-cache-dir', '--rm-cache-dir', '--dump-json'];
+    public $params = ['--simulate', '--no-check-certificate', '--no-cache-dir', '--rm-cache-dir'];
 
     /**
      * Enable audio conversion.
